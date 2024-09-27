5674f5706a8ff24fac387c9b763e599400fa6748
Hardik Dk <hardik.d.k121@gmail.com>
2024-09-27 21:26:11 +0530

    27-09-2024

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var back = document.getElementById('back');
+var forward = document.getElementById('forward');
+
+back.addEventListener('click',function () 
+    {
+        console.log("backbutton click kairu");
+    });
+    forward.addEventListener('click',function()
+    {
+        console.log("forward click kairu");
+    });
