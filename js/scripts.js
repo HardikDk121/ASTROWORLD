176405048c320f23865b0d97172411470ac26b11
Hardik Dk <hardik.d.k121@gmail.com>
2024-10-05 12:55:12 +0530

    05-10-2024

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
-/*var back = document.getElementById('back');
-var forward = document.getElementById('forward');
 
+const monthname = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const today = new Date();
+const currentMonth = monthname[today.getMonth()];
+const monthspan = document.getElementById('currentMonth');
+monthspan.textContent = currentMonth;
+
+var back = document.getElementById('back');
+var forward = document.getElementById('forward');
 back.addEventListener('click',function () 
+{
+
+    var month = monthname.indexOf(monthspan.textContent);
+    if (month == 0)
     {
-        console.log("backbutton click kairu");
-    });
+        
+    }
+    var previousmonth = monthname[month - 1];
+    monthspan.textContent = previousmonth;
+});
     forward.addEventListener('click',function()
-    {
-        console.log("forward click kairu");
-    });
-*/
+{
+    var month =monthname.indexOf(monthspan.textContent);
+    var nextmonth = monthname[month + 1];
+    monthspan.textContent = nextmonth;
+});
+
+
+
